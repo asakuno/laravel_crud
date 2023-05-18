1f6fde127574ae5a8384133cee720009c5217a02
asakuno <takahiro.asakuno@gmail.com>
2023-05-18 11:26:51 +0900

    [Add]デザイン追加

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -12,15 +12,32 @@
                 "Hiragio Sans",
                 Meiryo,
                 sans-serif;
+                background-image: url(/images/book_image.jpeg);
+                background-size: 100% auto;
             }
         </style>
         <title>book_shop</title>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
-      <div class="container">
-        @yield('content')
-      </div>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <div class=" bg-gray-100 py-4 sm:pt-0">
+        @if (Route::has('login'))
+            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                @auth
+                    <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
+                @else
+                    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                    @endif
+                @endauth
+            </div>
+        @endif
+    </div>
+    <div class="flex grow">
+      @yield('content')
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </body>
 </html>
--- src/resources/views/tops/index.blade.php
+++ src/resources/views/tops/index.blade.php
@@ -1,5 +1,17 @@
 @extends('app')
 
 @section('content')
-    <h1>About</h1>
+    <section class="min-h-96 flex flex-1 shrink-0 items-center justify-center overflow-hidden rounded-lg py-16 md:py-20 xl:py-48">
+        <div class="flex flex-col items-center p-4 sm:max-w-xl">
+            <h1 class="mb-5 text-center text-4xl font-bold text-gray-200 sm:text-5xl md:mb-12 md:text-5xl">
+                書店が好きな方に
+            </h1>
+            <h1 class="mb-5 text-center text-4xl font-bold text-gray-200 sm:text-5xl md:mb-12 md:text-5xl">
+                あなたのおすすめの書店を紹介してみませんか？
+            </h1>
+            <div class="flex justify-center">
+                <button class="btn btn-primary">試しに使ってみる</button>
+            </div>
+        </div>
+    </section>
 @endsection
