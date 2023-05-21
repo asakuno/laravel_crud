4538c851fbeae7a107a22b7ee668566475a7f05e
asakuno <takahiro.asakuno@gmail.com>
2023-05-21 13:54:26 +0900

    [Fix]インデントが崩れていたため週崩れていたため修正

--- src/resources/views/shops/index.blade.php
+++ src/resources/views/shops/index.blade.php
@@ -3,12 +3,12 @@
 @section('content')
     <div class="mx-5 my-5 bg-white" style="overflow-x: auto;">
         <div class="flex justify-center">
-                    <form action="{{ route('shops.index') }}" method="GET">
-                        <input type="text" name="keyword" value="{{ $keyword }}" placeholder="書店名 住所" class="input input-bordered bg-gray-100" />
-                        <button type="submit" value="検索" class="btn btn-outline btn-secondary">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-                        </button>
-                    </form>
+            <form action="{{ route('shops.index') }}" method="GET">
+                <input type="text" name="keyword" value="{{ $keyword }}" placeholder="書店名 住所" class="input input-bordered bg-gray-100" />
+                <button type="submit" value="検索" class="btn btn-outline btn-secondary">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                </button>
+            </form>
         </div>
         @auth
             <a href="{{ route('shop.create') }}" class="btn btn-outline btn-success mb-3">書店登録</a>
