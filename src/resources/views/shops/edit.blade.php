1e5e3da689801fed7c18cc849db107f86c74c289
asakuno <takahiro.asakuno@gmail.com>
2023-05-21 17:21:50 +0900

    [Fix]インデント

--- src/resources/views/shops/edit.blade.php
+++ src/resources/views/shops/edit.blade.php
@@ -1,61 +1,61 @@
 @extends('shops.app')
 
 @section('content')
-<div class="max-w-md mx-auto mt-10">
-    <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <div class="mb-4">
-            <h1 class="text-3xl font-bold underline">書店編集</h1>
-        </div>
-        <div class="text-align-right">
-            <form action="{{ route('shop.update', $shop->id) }}" method="POST">
-                @method('PUT')
-                @csrf
+    <div class="max-w-md mx-auto mt-10">
+        <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div class="mb-4">
+                <h1 class="text-3xl font-bold underline">書店編集</h1>
+            </div>
+            <div class="text-align-right">
+                <form action="{{ route('shop.update', $shop->id) }}" method="POST">
+                    @method('PUT')
+                    @csrf
 
-                <div class="row">
-                    <div class="col-12 mt-2 mb-2">
-                        <div class="form-group">
-                            <label for="name">書店名</label>
-                            <input type="text" value="{{ $shop->name }}" class="form-control" name="name" id="name" placeholder="書店名を入力してください">
-                            @error('name')
-                                <span style="color:red;">書店名を20文字以内で入力してください</span>
-                            @enderror
+                    <div class="row">
+                        <div class="col-12 mt-2 mb-2">
+                            <div class="form-group">
+                                <label for="name">書店名</label>
+                                <input type="text" value="{{ $shop->name }}" class="form-control" name="name" id="name" placeholder="書店名を入力してください">
+                                @error('name')
+                                    <span style="color:red;">書店名を20文字以内で入力してください</span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="col-12 mt-2 mb-2">
+                            <div class="form-group">
+                                <label for="address">住所</label>
+                                <input type="text" value="{{ $shop->address }}" class="form-control" name="address" id="address" placeholder="住所を入力してください">
+                                @error('address')
+                                    <span style="color:red;">住所を入力してください</span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div id="map"></div>
+
+                        <div class="col-12 mt-2 mb-2">
+                            <div class="form-group">
+                                <label for="description">詳細</label>
+                                <textarea value="{{ $shop->description }}" class="form-control" name="description" id="description" rows="3" placeholder="詳細"></textarea>
+                                @error('description')
+                                <span style="color:red;">詳細を140文字以内で入力してください</span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <input style="display:none;" type="text" id="latitude" name="latitude" value="{{ $shop->latitude }}"><br>
+
+                        <input style="display:none;" type="text" id="longitude" name="longitude" value="{{ $shop->longitude }}"><br>
+
+
+                        <div class="col-12 mt-2 mb-2">
+                            <button type="submit" class="btn btn-primary w-100">登録</button>
                         </div>
                     </div>
-
-                    <div class="col-12 mt-2 mb-2">
-                        <div class="form-group">
-                            <label for="address">住所</label>
-                            <input type="text" value="{{ $shop->address }}" class="form-control" name="address" id="address" placeholder="住所を入力してください">
-                            @error('address')
-                                <span style="color:red;">住所を入力してください</span>
-                            @enderror
-                        </div>
-                    </div>
-                    <div id="map"></div>
-
-                    <div class="col-12 mt-2 mb-2">
-                        <div class="form-group">
-                            <label for="description">詳細</label>
-                            <textarea value="{{ $shop->description }}" class="form-control" name="description" id="description" rows="3" placeholder="詳細"></textarea>
-                            @error('description')
-                              <span style="color:red;">詳細を140文字以内で入力してください</span>
-                            @enderror
-                        </div>
-                    </div>
-
-                    <input style="display:none;" type="text" id="latitude" name="latitude" value="{{ $shop->latitude }}"><br>
-
-                    <input style="display:none;" type="text" id="longitude" name="longitude" value="{{ $shop->longitude }}"><br>
-
-
-                    <div class="col-12 mt-2 mb-2">
-                        <button type="submit" class="btn btn-primary w-100">登録</button>
-                    </div>
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
     </div>
-</div>
 <script>
 function ChangeAddress() {
     // geocoderをを利用して位置情報を取得
--- src/resources/views/shops/show.blade.php
+++ src/resources/views/shops/show.blade.php
@@ -1,35 +1,35 @@
 @extends('shops.app')
 
 @section('content')
-<div class="max-w-md mx-auto mt-10">
-<a class="btn btn-success" href="{{ url('/shops') }}?page={{ $page_id }}">戻る</a>
-    <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <div class="text-align-right">
-            <div class="row">
-                <div class="col-12 mt-2 mb-2">
-                    <div class="flex relative items-center text-lg">
-                        <p class="font-bold tracking-wider">{{ $shop->name }}</p>
+    <div class="max-w-md mx-auto mt-10">
+        <a class="btn btn-success" href="{{ url('/shops') }}?page={{ $page_id }}">戻る</a>
+        <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div class="text-align-right">
+                <div class="row">
+                    <div class="col-12 mt-2 mb-2">
+                        <div class="flex relative items-center text-lg">
+                            <p class="font-bold tracking-wider">{{ $shop->name }}</p>
+                        </div>
                     </div>
-                </div>
 
-                <div class="col-12 mt-2 mb-2">
-                    <div class="mb-3">
-                        <label for="description" class="font-bold">詳細</label>
-                        <p class="py-2">{{ $shop->description }}</p>
+                    <div class="col-12 mt-2 mb-2">
+                        <div class="mb-3">
+                            <label for="description" class="font-bold">詳細</label>
+                            <p class="py-2">{{ $shop->description }}</p>
+                        </div>
                     </div>
-                </div>
 
-                <div class="col-12 mt-2 mb-2 pb-5 border-y-2 border-gray-200">
-                    <div class="mb-3 py-2">
-                        <label for="address" class="font-bold">住所</label>
-                        <p class="py-1">{{ $shop->address }}</p>
+                    <div class="col-12 mt-2 mb-2 pb-5 border-y-2 border-gray-200">
+                        <div class="mb-3 py-2">
+                            <label for="address" class="font-bold">住所</label>
+                            <p class="py-1">{{ $shop->address }}</p>
+                        </div>
+                        <div id="map" style="height: 300px; width: 380px;"></div>
                     </div>
-                    <div id="map" style="height: 300px; width: 380px;"></div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 <script>
 function initMap() {
     const mySpot = { lat: {{ $shop->latitude }}, lng: {{ $shop->longitude }} };
