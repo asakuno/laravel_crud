71a9a0adac3d1c506f4deaa9e86aa47d7f455978
asakuno <takahiro.asakuno@gmail.com>
2023-05-21 13:50:33 +0900

    [Fix]検索ボタンのデザイン修正

--- src/resources/views/shops/index.blade.php
+++ src/resources/views/shops/index.blade.php
@@ -2,13 +2,21 @@
 
 @section('content')
     <div class="mx-5 my-5 bg-white" style="overflow-x: auto;">
+        <div class="flex justify-center">
+                    <form action="{{ route('shops.index') }}" method="GET">
+                        <input type="text" name="keyword" value="{{ $keyword }}" placeholder="書店名 住所" class="input input-bordered bg-gray-100" />
+                        <button type="submit" value="検索" class="btn btn-outline btn-secondary">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                        </button>
+                    </form>
+        </div>
         @auth
             <a href="{{ route('shop.create') }}" class="btn btn-outline btn-success mb-3">書店登録</a>
         @endauth
         <table class="table table-bordered table-responsive">
             <tr>
                 <th>作成者</th>
-                <th>名前</th>
+                <th>書店名</th>
                 <th>住所</th>
                 <th>緯度</th>
                 <th>経度</th>
