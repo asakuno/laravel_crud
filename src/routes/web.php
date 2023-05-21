daefb80c9d1d36dffba0afcc3b884484c25c8824
asakuno <takahiro.asakuno@gmail.com>
2023-05-21 11:28:35 +0900

    [Fix]ルーティングによるcreateメソッドのエラーを修正

--- src/routes/web.php
+++ src/routes/web.php
@@ -19,20 +19,16 @@ Route::get('/', function () {
     return view('tops.index');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 Route::get('/shops', [ShopController::class, 'index'])->name('shops.index');
-Route::get('/shops/{shop}', [ShopController::class, 'show'])->name('shop.show');
+Route::get('/shops/{shop}', [ShopController::class, 'show'])->name('shop.show')->where('shop', '[0-9]+');
 Route::middleware('auth')->group(function () {
-    Route::get('shops/create', [ShopController::class, 'create'])->name('shop.create');
-    Route::post('shops/store', [ShopController::class, 'store'])->name('shop.store');
+    Route::get('/shops/create', [ShopController::class, 'create'])->name('shop.create');
+    Route::post('/shops/store', [ShopController::class, 'store'])->name('shop.store');
     Route::get('/shops/edit/{shop}', [ShopController::class, 'edit'])->name('shop.edit');
     Route::put('/shops/update/{shop}', [ShopController::class, 'update'])->name('shop.update');
     Route::delete('/shops/{shop}', [ShopController::class, 'destroy'])->name('shop.destroy');
