c269d695f866c9775414cee0ce234f7150916e90
asakuno <takahiro.asakuno@gmail.com>
2023-06-06 00:45:47 +0900

    [Add]コメントアウトの追加

--- src/app/Http/Controllers/ShopController.php
+++ src/app/Http/Controllers/ShopController.php
@@ -11,18 +11,16 @@ use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class ShopController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
+        //検索フィールドにあるkeywordの取得
         $keyword = request()->input('keyword');
+        //セレクトボックスにある県の取得
         $prefecture = request()->input('prefecture');
 
+        //Models/Shop.phpで定義したscopeSearchメソッドの呼び出し
         $query = Shop::latest()->search($keyword);
-        
+        //県の部分一致検索
         if (!empty($prefecture)) {
             $query->where('address', 'LIKE', "%{$prefecture}%");
         }
@@ -30,12 +28,14 @@ class ShopController extends Controller
         $shops = $query->orderBy('created_at', 'DESC')->paginate(5);
 
         $user = Auth::user();
+        //recommendedShopを定義しないとuser->profileがnullの時エラーが発生する
         $recommendedShop = null;
 
+        //プロフィール(現在地)を設定している時レコメンド
         if ($user && $user->profile && $user->profile->current_address) {
         $recommendedShop = Shop::where('address', 'LIKE', '%' . $user->profile->current_address . '%')
             ->inRandomOrder()
-            ->first();
+            ->first(); //ランダムで一件、レコメンドされたshopが表示される
         }
 
         return view('shops.index', compact('shops', 'keyword', 'recommendedShop', 'prefecture'))
@@ -43,22 +43,11 @@ class ShopController extends Controller
             ->with('i', (request()->input('page', 1) - 1) * 5);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view('shops.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(ShopRequest $request)
     {
         $shop = new Shop;
@@ -74,27 +63,15 @@ class ShopController extends Controller
             ->with('success', $shop->name.'を作成しました');
     }
 
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Shop  $shop
-     * @return \Illuminate\Http\Response
-     */
     public function show(Shop $shop)
     {
         return view('shops.show', compact('shop'))
             ->with('page_id',request()->page_id);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Shop  $shop
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Shop $shop, ShopService $shopService)
     {
+        //書店作成者が違う場合403エラーを返す
         if (!$shopService->checkOwnShop(Auth::user()->id, $shop->id)) {
             throw new AccessDeniedHttpException();
         }
@@ -102,16 +79,10 @@ class ShopController extends Controller
             ->with('page_id',request()->page_id);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Shop  $shop
-     * @return \Illuminate\Http\Response
-     */
     public function update(ShopRequest $request, ShopService $shopService)
     {
         $shop = Shop::find($request->route('shop'));
+        //書店作成者が違う場合403エラーを返す
         if (!$shopService->checkOwnShop($request->user()->id, $shop->id)) {
             throw new AccessDeniedHttpException();
         }
@@ -128,15 +99,10 @@ class ShopController extends Controller
             ->with('success', $shop->name.'を編集しました');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Shop  $shop
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Request $request, ShopService $shopService)
     {
         $shop = Shop::find($request->route('shop'));
+        //書店作成者が違う場合403エラーを返す
         if (!$shopService->checkOwnShop($request->user()->id, $shop->id)) {
             throw new AccessDeniedHttpException();
         }
@@ -155,8 +121,8 @@ class ShopController extends Controller
     public function favorite_shops()
     {
         $user = Auth::user();
+        //favorite_shopsの中にお気に入りした書店がある
         $shops = $user->favorite_shops()->orderBy('created_at', 'DESC')->paginate(5);
-
 
         return view('shops.favorites', compact('shops'));
     }
