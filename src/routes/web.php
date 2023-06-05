61330377264163438e84c8885a038542c3d906d0
asakuno <takahiro.asakuno@gmail.com>
2023-06-05 14:38:29 +0900

    [Add]controllerの作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ShopController;
 use App\Http\Controllers\ImageUploadController;
 use App\Http\Controllers\CommentController;
+use App\Http\Controllers\FavoriteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,6 +29,7 @@ Route::middleware('auth')->group(function () {
     Route::put('/profile/update', [ProfileController::class, 'profile_update'])->name('profile.profile_update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
 Route::get('/shops', [ShopController::class, 'index'])->name('shops.index');
 Route::get('/shops/map', [ShopController::class, 'map'])->name('shops.map');
 Route::get('/shops/{shop}', [ShopController::class, 'show'])->name('shop.show')->where('shop', '[0-9]+');
@@ -39,9 +41,10 @@ Route::middleware('auth')->group(function () {
     Route::delete('/shops/{shop}', [ShopController::class, 'destroy'])->name('shop.destroy');
     Route::post('shops/{shop}/comments', [CommentController::class, 'store'])->name('comment.store');
     Route::delete('comments/{comment}', [CommentController::class, 'destroy'])->name('comment.destroy');
-    Route::get('shops/favorite/{id}', [FavoriteController::class, 'store'])->name('favorite.store');
-    Route::delete('shops/favorite/{id}', [FavoriteController::class, 'destroy'])->name('favorite.destroy');
+    Route::get('shops/favorite/{shop}', [FavoriteController::class, 'store'])->name('favorite.store');
+    Route::delete('shops/favorite/{shop}', [FavoriteController::class, 'destroy'])->name('favorite.destroy');
 });
+
 Route::middleware('auth')->group(function () {
     Route::post('image-upload', [ ImageUploadController::class, 'imageUploadPost' ])->name('image.upload.post');
 });
